807b1e1de374d6815beaeddfbf2857301266e968
Alexander Ukwueze <contact@mail.lexiz.is-a.dev>
2026-06-01 13:24:02 +0100

    fix: enable CORS

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 import { env } from 'env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.setGlobalPrefix('api/v1', {
     exclude: [{ method: RequestMethod.GET, path: '/r/:id' }],
